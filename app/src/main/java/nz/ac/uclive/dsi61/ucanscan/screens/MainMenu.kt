60534f47c074654a843d366ad3cf244cddc8743a
Priscilla Ishida-Foale <pis19@uclive.ac.nz>
2023-09-22 18:07:31 +1200

    Created basic main screen with the start race, my times and setting buttons. Also added a placeholder logo. TODO: Add the bg image to the screen and make the buttons larger + string resources

--- app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/MainMenu.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/MainMenu.kt
@@ -2,16 +2,40 @@ package nz.ac.uclive.dsi61.ucanscan.screens
 
 import android.annotation.SuppressLint
 import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import nz.ac.uclive.dsi61.ucanscan.R
 import nz.ac.uclive.dsi61.ucanscan.navigation.Screens
 import nz.ac.uclive.dsi61.ucanscan.ui.theme.UCanScanTheme
 
@@ -25,25 +49,114 @@ fun MainMenuScreen(context: Context,
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        //TODO: replace the example code
-        Greeting("Priscilla")
 
-        // ROUGH BUTTON TO DEMONSTRATE NAVIGATION: REPLACE ME
-        Button(
-            onClick = {
-                navController.navigate(Screens.Race.route)
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(16.dp),
+//            verticalArrangement = Arrangement.Center,
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            Greeting("Priscilla")
+//            Spacer(modifier = Modifier.height(16.dp))
+//            Button(
+//                onClick = {
+//                    navController.navigate(Screens.Race.route)
+//                }
+//            ) {
+//                Text("Start Race")
+//            }
+//            Spacer(modifier = Modifier.height(16.dp))
+//            Button(
+//                onClick = {
+//                    // Handle the second button click here
+//                }
+//            ) {
+//                Text("My Times")
+//            }
+//        }
+
+        Scaffold(
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        modifier = Modifier.size(size = 150.dp).clip(CircleShape)
+                            .border(width = 2.dp, color = Color.White, shape = CircleShape),
+                        painter = painterResource(id = R.drawable.cat_one),
+                        contentDescription = "Placeholder Logo"
+
+                    )
+                    Greeting("Priscilla")
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            navController.navigate(Screens.Race.route)
+                        }
+                    ) {
+                        Text("Start Race")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                        }
+                    ) {
+                        Text("My Times")
+                    }
+                }
+            },
+            bottomBar = {
+                BottomAppBar(
+                    containerColor = Color.Transparent,
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+
+                    Button(
+                        onClick = {
+                        },
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                modifier = Modifier.size(20.dp),
+                                imageVector = Icons.Outlined.Settings,
+                                contentDescription = ""
+                            )
+                            Spacer(modifier = Modifier.width(width = 8.dp))
+                            Text(text = "Settings")
+                        }
+                    }
+                    Spacer( // push the button to the left side of the screen
+                        modifier = Modifier.weight(1f)
+                    )
+                }
             }
-        ) {
-            // empty content
-        }
+        )
+//        //TODO: replace the example code
+//        Greeting("Priscilla")
+//
+//        // ROUGH BUTTON TO DEMONSTRATE NAVIGATION: REPLACE ME
+//        Button(
+//            onClick = {
+//                navController.navigate(Screens.Race.route)
+//            }
+//        ) {
+//            // empty content
+//        }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Welcome, $name!",
+        modifier = modifier,
+        fontSize = 30.sp
+
     )
 }
 
