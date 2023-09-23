6a9028f4516b1b0a88e25170ba5683ef99787376
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-09-24 12:07:15 +1300

    Added map icon and fixed styling of buttons

--- app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Race.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Race.kt
@@ -1,33 +1,25 @@
 package nz.ac.uclive.dsi61.ucanscan.screens
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MailOutline
+
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
-import androidx.core.app.ComponentActivity
-import androidx.core.content.ContextCompat
+
 import androidx.navigation.NavController
 import nz.ac.uclive.dsi61.ucanscan.R
 
@@ -42,59 +34,46 @@ fun RaceScreen(context: Context,
         color = MaterialTheme.colorScheme.background
     ) {
 
-        Column(
+        Row(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-
-            val cameraLauncher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                //handle image scanning
-            }
-
-            val context = LocalContext.current
-
-            IconButton(
-                onClick = {
-                    if (checkCameraPermission(context)) {
-                        val cameraIntent = android.content.Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
-                        cameraLauncher.launch(cameraIntent)
-                    } else {
-                        requestCameraPermission(context)
-                    }
-                },
+            
+            Button(
                 modifier = Modifier
-                    .size(100.dp)
-                    .background(Color.White, shape = RoundedCornerShape(percent = 50))
+                    .size(100.dp),
+                shape = RoundedCornerShape(16.dp),
+                onClick = {
+                    },
             ) {
-
                 Icon(
                     painter = painterResource(id = R.drawable.camera),
-                    contentDescription = "Camera"
+                    contentDescription = "Camera",
+                    modifier = Modifier
+                        .size(100.dp)
                 )
             }
 
 
-            IconButton(
+
+            Button(
+                modifier = Modifier
+                    .size(100.dp),
+                shape = RoundedCornerShape(16.dp),
                 onClick = {
-                    //map btn click
-                }
+                    //TODO handle map btn onclick
+                     },
             ) {
                 Icon(
-                    imageVector = Icons.Default.MailOutline,
-                    contentDescription = "Map Icon",
-                    tint = Color.Black
+                    painter = painterResource(id = R.drawable.map),
+                    contentDescription = "Map",
+                    modifier = Modifier
+                        .size(100.dp)
                 )
             }
+
+
         }
     }
-}
-
-private fun checkCameraPermission(context: Context): Boolean {
-    val result = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
-    return result == PackageManager.PERMISSION_GRANTED
-}
-
-private fun requestCameraPermission(context: Context) {
-    ActivityCompat.requestPermissions(context as ComponentActivity, arrayOf(Manifest.permission.CAMERA), 0)
 }
