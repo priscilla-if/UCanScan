beec1b8fa12bbeaf6d4be66c913318a8f339c27e
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-09-24 16:40:54 +1300

    Fixed race screen page styling and added strings resources for text

--- app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Race.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Race.kt
@@ -3,6 +3,7 @@ package nz.ac.uclive.dsi61.ucanscan.screens
 import android.annotation.SuppressLint
 import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
@@ -16,9 +17,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 import androidx.navigation.NavController
 import nz.ac.uclive.dsi61.ucanscan.R
@@ -34,46 +45,90 @@ fun RaceScreen(context: Context,
         color = MaterialTheme.colorScheme.background
     ) {
 
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            
-            Button(
-                modifier = Modifier
-                    .size(100.dp),
-                shape = RoundedCornerShape(16.dp),
-                onClick = {
-                    },
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.camera),
-                    contentDescription = "Camera",
-                    modifier = Modifier
-                        .size(100.dp)
-                )
-            }
+     Column(
+         modifier = Modifier.fillMaxSize().padding(top = 100.dp),
+         verticalArrangement = Arrangement.Center,
+         horizontalAlignment = Alignment.CenterHorizontally
+     ) {
+
+         Text( modifier = Modifier.padding(bottom = 70.dp) ,
+             text = stringResource(id = R.string.next_landmark),
+             style = TextStyle(
+                 fontSize = 28.sp,
+                 fontWeight = FontWeight.Bold
+             )
+         )
 
 
 
-            Button(
-                modifier = Modifier
-                    .size(100.dp),
-                shape = RoundedCornerShape(16.dp),
-                onClick = {
-                    //TODO handle map btn onclick
-                     },
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.map),
-                    contentDescription = "Map",
-                    modifier = Modifier
-                        .size(100.dp)
-                )
-            }
+         Box(
+             modifier = Modifier
+                 .size(300.dp)
+                 .background(Color.Gray, shape = CircleShape)
+         ) {
+             Text(
+                 text = stringResource(id = R.string.jack_erskine_landmark),
+                 color = Color.Black,
+                 fontSize = 28.sp,
+                 textAlign = TextAlign.Center,
+                 modifier = Modifier
+                     .fillMaxSize()
+                     .padding(vertical = 130.dp)
+                     .align(Alignment.Center),
+                 style = TextStyle(
+                     fontWeight = FontWeight.Bold
+             )
+             )
+         }
 
 
-        }
+
+         Row(
+             modifier = Modifier.fillMaxSize(),
+             verticalAlignment = Alignment.CenterVertically,
+             horizontalArrangement = Arrangement.SpaceEvenly
+         ) {
+
+             Button(
+                 modifier = Modifier
+                     .size(100.dp),
+                 shape = RoundedCornerShape(16.dp),
+                 onClick = {
+                 },
+             ) {
+                 Icon(
+                     painter = painterResource(id = R.drawable.camera),
+                     contentDescription = "Camera",
+                     modifier = Modifier
+                         .size(100.dp)
+                 )
+             }
+
+
+
+             Button(
+                 modifier = Modifier
+                     .size(100.dp),
+                 shape = RoundedCornerShape(16.dp),
+                 onClick = {
+                     //TODO handle map btn onclick
+                 },
+             ) {
+                 Icon(
+                     painter = painterResource(id = R.drawable.map),
+                     contentDescription = "Map",
+                     modifier = Modifier
+                         .size(100.dp)
+                 )
+             }
+
+
+         }
+
+
+     }
+
+
+
     }
 }
