ef46e7a8d8ec706ac33f6e6a4795eeb69dcdc17e
Danielle Sim <dsi61@uclive.ac.nz>
2023-09-25 13:41:47 +1300

    [feature] Add the rest of the settings.

--- app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Preferences.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Preferences.kt
@@ -2,18 +2,23 @@ package nz.ac.uclive.dsi61.ucanscan.screens
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.widget.ToggleButton
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -21,10 +26,13 @@ import androidx.compose.material3.SwitchColors
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MovableContent
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,24 +49,28 @@ import nz.ac.uclive.dsi61.ucanscan.navigation.Screens
 fun PreferencesScreen(context: Context,
                navController: NavController) {
 
-    val switchState = remember {
-        mutableStateOf(true)
-    }
-
+    val notificationOption1State = remember { mutableStateOf(true) }
+    val notificationOption2State = remember { mutableStateOf(true) }
+    val notificationOption3State = remember { mutableStateOf(true) }
+    val themeOption1State = remember { mutableStateOf(true) }
+    val animationOption1State = remember { mutableStateOf(true) }
+    val animationOption2State = remember { mutableStateOf(true) }
+    var selectedUserName by remember { mutableStateOf("we have no DB yet") } //TODO: get value from db
 
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
         Column(
-            //horizontalArrangement = Arrangement.Center
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = stringResource(R.string.preferences)
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
 
             Column(
                 //
@@ -88,8 +100,8 @@ fun PreferencesScreen(context: Context,
                     )
                     // https://betterprogramming.pub/create-an-android-switch-using-jetpack-compose-351eddbf5828
                     Switch(
-                        checked = switchState.value,
-                        onCheckedChange = { switchState.value = it }
+                        checked = notificationOption1State.value,
+                        onCheckedChange = { notificationOption1State.value = it } //TODO: save to db
                     )
                 }
 
@@ -108,8 +120,8 @@ fun PreferencesScreen(context: Context,
                         modifier = Modifier.weight(1f)
                     )
                     Switch(
-                        checked = switchState.value,
-                        onCheckedChange = { switchState.value = it }
+                        checked = notificationOption2State.value,
+                        onCheckedChange = { notificationOption2State.value = it } //TODO: save to db
                     )
                 }
 
@@ -128,8 +140,8 @@ fun PreferencesScreen(context: Context,
                         modifier = Modifier.weight(1f)
                     )
                     Switch(
-                        checked = switchState.value,
-                        onCheckedChange = { switchState.value = it }
+                        checked = notificationOption3State.value,
+                        onCheckedChange = { notificationOption3State.value = it } //TODO: save to db
                     )
                 }
 
@@ -143,6 +155,26 @@ fun PreferencesScreen(context: Context,
                     )
                 }
 
+                // option 1
+                Row(
+                    //
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = stringResource(R.string.prefs_theme_opt1)
+                        )
+                    }
+                    Spacer(
+                        modifier = Modifier.weight(1f)
+                    )
+                    Switch(
+                        checked = themeOption1State.value,
+                        onCheckedChange = { themeOption1State.value = it } //TODO: save to db
+                    )
+                }
+
 
                 // ANIMATIONS SECTION
                 Row(
@@ -153,50 +185,81 @@ fun PreferencesScreen(context: Context,
                     )
                 }
 
+                // option 1
+                Row(
+                    //
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = stringResource(R.string.prefs_animations_opt1)
+                        )
+                    }
+                    Spacer(
+                        modifier = Modifier.weight(1f)
+                    )
+                    Switch(
+                        checked = animationOption1State.value,
+                        onCheckedChange = { animationOption1State.value = it } //TODO: save to db
+                    )
+                }
+
+                // option 2
+                Row(
+                    //
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = stringResource(R.string.prefs_animations_opt2)
+                        )
+                    }
+                    Spacer(
+                        modifier = Modifier.weight(1f)
+                    )
+                    Switch(
+                        checked = animationOption2State.value,
+                        onCheckedChange = { animationOption2State.value = it } //TODO: save to db
+                    )
+                }
+
+
+                // NAME SECTION
+                Row(
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = stringResource(R.string.prefs_name).uppercase(),
+                    )
+                }
+
+                // option 1
+                Row(
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = stringResource(R.string.prefs_name_opt1)
+                    )
+                }
+                Row(
+                    //
+                ) {
+                    TextField(
+                        value = selectedUserName,
+                        onValueChange = {
+                            selectedUserName = it
+                        },
+                    )
+                }
+
+
             }
         }
-
 
 
     }
 }
 
-// https://developer.android.com/reference/kotlin/androidx/compose/material3/package-summary#switch
-//@Composable
-//fun Switch(
-//    checked: Boolean,
-//    onCheckedChange: ((Boolean) -> Unit)?,
-//    modifier: Modifier = Modifier,
-//    thumbContent: (@Composable () -> Unit)? = null,
-//    enabled: Boolean = true,
-//    colors: SwitchColors = SwitchDefaults.colors(),
-//    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
-//): Unit {
-//
-//}
 
-//fun Switch(
-//    modifier = Modifier.semantics { contentDescription = "Demo with icon" },
-//    checked = checked,
-//    onCheckedChange = { checked = it },
-//    thumbContent = icon
-//) {
-//
-//}
-
-
-
-//@Composable
-//fun Switch(
-//    checked: Boolean = isChecked,
-//    onCheckedChange: () -> Unit ={isChecked.value=it},
-//    colors: SwitchColors = SwitchDefaults.colors(
-//        checkedThumbColor = Color.DarkGray,
-//        uncheckedThumbColor = Color.DarkGray,
-//        checkedTrackColor = Color.Blue,
-//        uncheckedTrackColor = Color.Blue,
-//    )
-//) {
-//
-//
-//}
