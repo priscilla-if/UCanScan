06819b91a5a5318757fc5c0cf1c67c4eba01a4a5
Danielle Sim <dsi61@uclive.ac.nz>
2023-09-27 17:33:25 +1300

    Added sample landmarks to the map: the camera gets focused on the upciming landmark, all landmarks have markers, and they are colour-coded according to found/not found.

---
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/Landmark.kt
@@ -0,0 +1,8 @@
+package nz.ac.uclive.dsi61.ucanscan
+
+// TODO: TEMPORARY CLASS FOR TESTING THE MAPS: REPLACE WITH ROOMDB VERSION WHEN ADDED
+class Landmark(
+    val lat: Double,
+    val lng: Double,
+    val isFound: Boolean) {
+}
--- app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Map.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Map.kt
@@ -2,20 +2,29 @@ package nz.ac.uclive.dsi61.ucanscan.screens
 
 import android.annotation.SuppressLint
 import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import nz.ac.uclive.dsi61.ucanscan.Landmark
+import nz.ac.uclive.dsi61.ucanscan.R
 import nz.ac.uclive.dsi61.ucanscan.navigation.Screens
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,28 +32,68 @@ import nz.ac.uclive.dsi61.ucanscan.navigation.Screens
 @Composable
 fun MapScreen(context: Context,
                navController: NavController) {
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+
+    var cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(0.0, 0.0), 16f)
     }
+
+    //TODO: replace with RoomDB landmarks
+    val sampleLandmarks = mutableListOf<Landmark>(
+        Landmark(-43.52256, 172.58119, true),
+        Landmark(-43.52257, 172.58268, true),
+        Landmark(-43.52456, 172.58351, true),
+        Landmark(-43.52698, 172.58451, true),
+        Landmark(-43.52435, 172.58079, false)
+    )
+
+    for(landmark in sampleLandmarks) {
+        // only one landmark at a time isn't found. Set the camera on this landmark.
+        if(!landmark.isFound) {
+            cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(LatLng(landmark.lat, landmark.lng), 16f)
+            }
+        }
+    }
+
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
     ) {
-        Marker(
-            state = MarkerState(position = singapore),
-            title = "Singapore",
-            snippet = "Marker in Singapore"
-        )
+        for(landmark in sampleLandmarks) {
+            if(landmark.isFound) {
+                Marker( // green markers for visited landmarks
+                    state = MarkerState(position = LatLng(landmark.lat, landmark.lng)),
+                    title = "Jack Erskine",
+                    snippet = "This is a marker",
+                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
+                )
+            } else {
+                Marker( // red marker for the one upcoming landmark
+                    state = MarkerState(position = LatLng(landmark.lat, landmark.lng)),
+                    title = "Jack Erskine",
+                    snippet = "This is a marker",
+                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+                )
+            }
+        }
+
     }
 
+    Row(
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = {
+                navController.navigate(Screens.MainMenu.route)
+            },
+            modifier = Modifier.size(width = 200.dp, height = 130.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.back_to_race),
+                fontSize = 20.sp
+            )
+        }
+    }
 
-
-
-
-
-
-
-    //TODO: add components for app buttons off of the map
 
 }
