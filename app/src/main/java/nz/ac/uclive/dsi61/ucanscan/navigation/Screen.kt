556e5a4f67d927e2f4e4935001e5b344139b80f7
Priscilla Ishida-Foale <pis19@uclive.ac.nz>
2023-10-05 11:00:59 +1300

    Fixed the redundant string issue

--- app/src/main/java/nz/ac/uclive/dsi61/ucanscan/navigation/Screen.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/navigation/Screen.kt
@@ -9,7 +9,7 @@ sealed class Screens(val route: String, @StringRes val resourceId: Int) {
 
     object Landmarks: Screens("landmarks", R.string.landmarks_screen)
 
-    object Preferences: Screens("preferences", R.string.preferences_screen)
+    object Preferences: Screens("preferences", R.string.preferences)
 
     object Leaderboard: Screens("leaderboard", R.string.leaderboard_screen)
 
