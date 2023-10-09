b4077c03ebe31b8db952e749e5838f0527015d83
Priscilla Ishida-Foale <pis19@uclive.ac.nz>
2023-10-09 13:16:20 +1300

    Missing import commit

--- app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Camera.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/screens/Camera.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
