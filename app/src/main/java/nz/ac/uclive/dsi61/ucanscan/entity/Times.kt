038a84d22a4103d943ee5e2aa9ea36d8ca28a100
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-10-10 19:16:51 +1300

    Fixed date issue for Times entity - was using mm instead of MM (which meant minutes not months !)

--- app/src/main/java/nz/ac/uclive/dsi61/ucanscan/entity/Times.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/entity/Times.kt
@@ -9,7 +9,7 @@ import java.util.Date
 
 @Entity(tableName = "times")
 class Times (
-    @ColumnInfo var dateAchieved: String = SimpleDateFormat("dd-mm-YYYY").format(Date()),
+    @ColumnInfo var dateAchieved: String = SimpleDateFormat("dd-MM-YYYY").format(Date()),
     @ColumnInfo var endTime: Long) {
 
     @PrimaryKey(autoGenerate = true) var id: Long = 0
