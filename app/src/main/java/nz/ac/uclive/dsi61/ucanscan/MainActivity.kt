2a4eadeed0293f0c191f210b1123c1f6f5b05a32
Danielle Sim <dsi61@uclive.ac.nz>
2023-10-11 23:56:36 +1300

    Merge conflicts & comments/whitespace

--- app/src/main/java/nz/ac/uclive/dsi61/ucanscan/MainActivity.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/ucanscan/MainActivity.kt
@@ -65,8 +65,6 @@ class MainActivity : ComponentActivity() {
         unregisterReceiver(AlarmReceiver)
     }
 
-
-
     private fun createNotificationChannel() {
         Log.d("FOO", "MainActivity createNotificationChannel!")
         val importance = NotificationManager.IMPORTANCE_DEFAULT //TODO: change?
@@ -77,59 +75,5 @@ class MainActivity : ComponentActivity() {
         notificationManager.createNotificationChannel(channel)
     }
 
-
-
-
-    // STEP 8
-//    override fun onTimeSet(picker: TimePicker, hour: Int, minute: Int) {
-//        val today = Calendar.getInstance().apply {
-//            timeInMillis = System.currentTimeMillis()
-//            set(Calendar.HOUR_OF_DAY, hour)
-//            set(Calendar.MINUTE, minute)
-//        }
-//
-//        val intent = Intent(applicationContext, AlarmReceiver::class.java).let {
-//            PendingIntent.getBroadcast(applicationContext, 0, it, 0)
-//        }
-//
-//        val alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-//        alarmManager.setInexactRepeating(AlarmManager.RTC, today.timeInMillis, AlarmManager.INTERVAL_DAY, intent)
-
-//        // STEP 11
-//        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-//        prefs.edit().apply {
-//            putInt("hour", hour)
-//            putInt("minute", minute)
-//            apply()
-//        }
-        //Utilities.scheduleReminder(applicationContext, hour, minute)
-        // Calculate the time for the next day at midnight
-//        val calendar = Calendar.getInstance().apply {
-//            timeInMillis = System.currentTimeMillis()
-//            set(Calendar.HOUR_OF_DAY, 0)
-//            set(Calendar.MINUTE, 0)
-//            set(Calendar.SECOND, 0)
-//            add(Calendar.DAY_OF_YEAR, 1) // Add 1 day to get the next day
-//        }
-//
-//        val intent = Intent(context, AlarmReceiver::class.java).let {
-//            PendingIntent.getBroadcast(context, 0, it, 0)
-//        }
-//
-//        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-//        alarmManager.setInexactRepeating(AlarmManager.RTC, today.timeInMillis, AlarmManager.INTERVAL_DAY, intent)
-//
-//
-
-
-//        // STEP 13: set the boot receiver to enabled
-//        val receiver = ComponentName(this, BootReceiver::class.java)
-//        packageManager.setComponentEnabledSetting(receiver, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP)
-    //}
-
-
-
-
-    //TODO: setting to set what hour the daily notif comes at
 
 }
